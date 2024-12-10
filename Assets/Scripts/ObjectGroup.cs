19c0a5906dd4da2a9cdb99be09897be007fcd69a
equaLize <LiDaniil@outlook.com>
2024-12-10 16:54:09 +0300

    Object snap setup

---
+++ Assets/Scripts/ObjectGroup.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObjectGroup : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _objects;
+
+    private int _index = 0;
+
+    private void Start()
+    {
+        foreach (GameObject obj in GetComponentsInChildren<GameObject>())
+            _objects.Add(obj.gameObject);
+    }
+
+    public void SnapObject()
+    {
+        _objects[_index].SetActive(false);
+        if (++_index >= _objects.Count)
+            _index = 0;
+        _objects[_index].SetActive(true);
+    }
+}
